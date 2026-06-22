4e137233549b2122774519842aaba8a657e011ca
MeetNavdiya1 <meetnavadiya7272@gmail.com>
2026-06-22 17:50:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    return res.json({
+        massage: "Hello from the server"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})
