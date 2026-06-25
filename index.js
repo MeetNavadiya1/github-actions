2639045b9491a1a1e60de06e47d2bf4d3d8f6b9c
MeetNavdiya1 <meetnavadiya7272@gmail.com>
2026-06-25 19:22:48 +0530

    updated the health check

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     return res.json({
-        massage: "Hello from the server v2 deploy"
+        massage: "Hello from the server v3 deploy with health check"
     })
 })
+
+app.get("/health", (req, res) => {
+  return res.json({
+    status: 200,
+    message: "OK",
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
